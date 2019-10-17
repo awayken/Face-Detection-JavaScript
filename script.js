210d51b3637a0bab0d0d6d67f3cf3f393ca0971b
Nick Williams <Nick.Williams@carsforsale.com>
2019-10-17 09:02:49 -0500

    emojis instead of canvas

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const video = document.getElementById('video')
+const emoji_img = document.getElementById('expression_emoji')
 
 Promise.all([
   faceapi.nets.tinyFaceDetector.loadFromUri('/models'),
@@ -14,7 +15,7 @@ function startVideo() {
     err => console.error(err)
   )
 }
-
+/*
 video.addEventListener('play', () => {
   const canvas = faceapi.createCanvasFromMedia(video)
   document.body.append(canvas)
@@ -23,9 +24,31 @@ video.addEventListener('play', () => {
   setInterval(async () => {
     const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions()
     const resizedDetections = faceapi.resizeResults(detections, displaySize)
+    console.log(resizedDetections.expressions)
     canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)
     faceapi.draw.drawDetections(canvas, resizedDetections)
     faceapi.draw.drawFaceLandmarks(canvas, resizedDetections)
     faceapi.draw.drawFaceExpressions(canvas, resizedDetections)
   }, 100)
+})
+*/
+const emotion_dict = {
+  'happy': "./happy.png",
+  'sad': "./sad.png",
+  "surprised": "./surprised.png",
+  "angry": "./angry.png",
+  "fearful": "./fearful.png",
+  "neutral": "./neutral.png",
+  "disgusted": "./disgusted.png"
+}
+video.addEventListener('play', () =>
+{
+  setInterval(async () => {
+    const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions()
+    //const resizedDetections = faceapi.resizeResults(detections, displaySize)
+    console.log(detections[0]["expressions"].asSortedArray()[0].expression)
+    const highest_prob = detections[0]["expressions"].asSortedArray()[0].expression
+    //const emotions = detections[0]["expressions"];
+    emoji_img.src = emotion_dict[highest_prob]
+  }, 1000)
 })
