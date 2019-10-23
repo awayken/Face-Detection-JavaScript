d2c4dde63591a9debeb433458e3f07bac6ac6362
Miles Rausch <miles.rausch@carsforsale.com>
2019-10-23 00:10:39 -0500

    Try to use media recorder

--- scripts/app.js
+++ scripts/app.js
@@ -2,9 +2,10 @@ let animationTimer = null;
 let faceDetectionSupported = false;
 let numberOfFaces = 0;
 let emojiBoxes = [];
+let mediaRecorder = null;
 
 const video = document.getElementById('webcam');
-const emoji_img = document.getElementById('expression_emoji');
+const snap = document.getElementById('snap');
 
 const emojiMap = {
     happy: '😄',
@@ -82,8 +83,51 @@ function animateFace() {
     });
 }
 
+function startSnapshot() {
+    if (mediaRecorder.state !== 'recording') {
+        mediaRecorder.start();
+    }
+
+    mediaRecorder.requestData();
+    mediaRecorder.stop();
+}
+
+function takeSnapshot(event) {
+    const snapshotBlob = event.data;
+
+    console.log(snapshotBlob.size, snapshotBlob.type);
+
+    var reader = new FileReader();
+    reader.addEventListener('loadend', function() {
+    // reader.result contains the contents of blob as a typed array
+        console.log(reader.result);
+    });
+    reader.readAsDataURL(snapshotBlob);
+}
+
 function startVideo() {
-    navigator.mediaDevices.getUserMedia({ audio: false, video: { facingMode: 'user' } }).then(stream => video.srcObject = stream).catch(err => console.error(err))
+    navigator.mediaDevices.getUserMedia({
+        audio: false,
+        video: { facingMode: 'user' }
+    })
+    .then(stream => {
+        video.srcObject = stream;
+
+        video.addEventListener('play', animateFace);
+        video.addEventListener('pause', () => cancelAnimationFrame(animationTimer));
+        video.addEventListener('error', () => cancelAnimationFrame(animationTimer));
+
+        return stream;
+    })
+    .then(stream => {
+        mediaRecorder = new MediaRecorder(stream);
+        mediaRecorder.ondataavailable = takeSnapshot;
+
+        snap.addEventListener('click', startSnapshot);
+
+        return stream;
+    })
+    .catch(err => console.error(err))
 }
 
 function everythingWorked() {
@@ -97,13 +141,11 @@ function somethingFailed(reason) {
     startVideo();
 }
 
-video.addEventListener('play', animateFace);
-video.addEventListener('pause', () => cancelAnimationFrame(animationTimer));
-video.addEventListener('error', () => cancelAnimationFrame(animationTimer));
-
 Promise.all([
     faceapi.nets.tinyFaceDetector.loadFromUri('./models'),
     faceapi.nets.faceRecognitionNet.loadFromUri('./models'),
     faceapi.nets.faceLandmark68Net.loadFromUri('./models'),
     faceapi.nets.faceExpressionNet.loadFromUri('./models')
-]).then(everythingWorked).catch(somethingFailed);
+])
+.then(everythingWorked)
+.catch(somethingFailed);
